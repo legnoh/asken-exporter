af18111ee9f1f3a583b04efbdfa8a7e818f1fddb
Ryoma Kai <706834+legnoh@users.noreply.github.com>
2024-12-16 13:08:45 +0900

    reduce mem usage

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -1,8 +1,5 @@
 #!/bin/bash
 set -e
 
-# Execute Display settings
-Xvfb :99 &
-
 # Then exec the container's main process (what's set as CMD in the Dockerfile).
 exec "$@"
--- main.py
+++ main.py
@@ -2,7 +2,7 @@ import logging, os, time, sys
 from selenium import webdriver
 from selenium.webdriver.chrome.service import Service
 from prometheus_client import CollectorRegistry, Gauge, Info, start_http_server
-
+from pyvirtualdisplay import Display
 import modules.asken as asken
 
 log_format = '%(asctime)s[%(filename)s:%(lineno)d][%(levelname)s] %(message)s'
@@ -17,15 +17,6 @@ if __name__ == '__main__':
 
     logging.info("# initializing chromium options...")
     options = webdriver.ChromeOptions()
-    driver = webdriver.Chrome(service=Service(), options=options)
-    driver.implicitly_wait(0.5)
-
-    logging.info("# login to asken...")
-    username = os.environ['ASKEN_USERNAME']
-    password = os.environ['ASKEN_PASSWORD']
-    ak_driver = asken.login(driver, username, password)
-    if ak_driver == None:
-        sys.exit(1)
 
     today = time.strftime("%Y-%m-%d")
 
@@ -40,10 +31,6 @@ if __name__ == '__main__':
         { 'name': 'weekly', 'path': '/1', 'desc': '過去7日の平均' },
         { 'name': 'monthly', 'path': '/2', 'desc': '月平均' },
     ]
-
-    # premium?
-    logging.info("# checking asken premium...")
-    premium = asken.is_premium(ak_driver)
 
     # create metrics
     logging.info("# create metrics instances...")
@@ -62,19 +49,39 @@ if __name__ == '__main__':
 
     while True:
 
-        # get daily score
+        if os.path.isfile("/.dockerenv"):
+            logging.info("# start display...")
+            display = Display(visible=0, size=(1024, 768))
+            display.start()
+        
+        logging.info("# start selenium...")
+        driver = webdriver.Chrome(service=Service(), options=options)
+        driver.implicitly_wait(0.5)
+
+        logging.info("# login to asken...")
+        username = os.environ['ASKEN_USERNAME']
+        password = os.environ['ASKEN_PASSWORD']
+        driver = asken.login(driver, username, password)
+        if driver == None:
+            sys.exit(1)
+
+        logging.info("# checking asken premium...")
+        premium = asken.is_premium(driver)
+
         logging.info("# getting daily score...")
-        daily_score = asken.get_latest_daily_score(ak_driver)
+        daily_score = asken.get_latest_daily_score(driver)
         score_gauge.set(daily_score)
 
-        # get advice
         logging.info("# getting advices...")
         for target in advice_targets:
-            advice = asken.get_advice(ak_driver, target['path'], premium)
+            advice = asken.get_advice(driver, target['path'], premium)
             metrics[target['name']]['advice'].info( { "advice": advice } )
 
-            detail_advice = asken.get_detail_advice(ak_driver)
+            detail_advice = asken.get_detail_advice(driver)
             metrics[target['name']]['detail_advice'].info( { "advice": detail_advice } )
 
         logging.info("# scraping account is successfully.")
+        driver.quit()
+        if os.path.isfile("/.dockerenv"):
+            display.stop()
         time.sleep(3600*4)
